b72f05e9385347cddb3744e0735e344748ba83ca
Milena27Alves <silvamilena140@gmail.com>
2021-01-31 12:50:21 -0300

    Projeto Spring

---
+++ src/main/java/com/example/Sistemadegerencimantodeloja/SistemaDeGerencimantoDeLojaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Sistemadegerencimantodeloja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeGerencimantoDeLojaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeGerencimantoDeLojaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Sistemadegerencimantodeloja/SistemaDeGerencimantoDeLojaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Sistemadegerencimantodeloja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeGerencimantoDeLojaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
