77ae8954d97a2bafca6db9ba12fe229a177d340c
Milena27Alves <silvamilena140@gmail.com>
2021-04-25 18:31:40 -0300

    Teste aplication context

--- src/test/java/com/example/Sistemadegerencimantodeloja/testesFuncionais/CucumberSpringConfiguration.java
+++ src/test/java/com/example/Sistemadegerencimantodeloja/testesFuncionais/CucumberSpringConfiguration.java
@@ -16,7 +16,7 @@ import org.springframework.test.context.support.AnnotationConfigContextLoader;
 //        Runner.class},
 //        webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @RunWith(SpringRunner.class)
-@ContextConfiguration(classes = SistemaDeGerencimantoDeLojaApplication.class,
+@ContextConfiguration(classes = SistemaDeGerencimantoDeLojaApplicationTests.class,
         loader = AnnotationConfigContextLoader.class
 )
 
